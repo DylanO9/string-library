69a388df0fa66f38788ada8d7e7bca60b2a00491
DylanO9 <dylan.olivares11@gmail.com>
2025-11-07 11:19:40 +0000

    fix: give it a correct test case

--- test/test_strlen.c
+++ test/test_strlen.c
@@ -1,8 +1,10 @@
 #include <assert.h>
 #include "../include/stringlib.h"
+#include <stdio.h>
 
 int main() {
     assert(my_strlen("hi") == 2);
+    printf("Everything looks good!\n");
     
     return 0;
 }
